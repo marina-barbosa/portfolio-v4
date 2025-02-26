c04ffc4820f5557284d7c1c388155629df032e1c
marina-barbosa <marinabarbosa.exp@gmail.com>
2025-02-26 18:49:27 -0300

    fix: caminho do icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { Page1 } from './components/opc1/page1'
-import { Embralhar } from './components/opc2/embaralhar'
-import { Glitch } from './components/opc3/glitch'
+const App = () => {
+  return <>halo</>;
+};
 
-function App() {
-  return (
-    <>
-      {/* <StarWindowEffect /> */}
-      {/* <Page1 /> */}
-     {/* <Embralhar /> */}
-     <Glitch/>
-    </>
-)  
-}
-
-export default App
+export default App;
