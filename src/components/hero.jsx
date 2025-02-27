21f1f865c9659c5d47c56a9dff833cd7765f82cf
marina-barbosa <marinabarbosa.exp@gmail.com>
2025-02-27 00:33:31 -0300

    feat: add button

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,17 @@
+import { ButtonCosmic } from "./button-cosmic";
 export const Hero = () => {
     return (
-      <section className="mx-auto flex items-center justify-center bg-gray-100  ">
-        <h1 className=" mx-auto max-w-[78vw] m-10 mt-40 text-7xl font-raleway uppercase font-bold md:text-left text-center">the future <br/>of extended reality<br/>2023</h1>
+      <section className="mx-auto h-screen flex flex-col items-center justify-center bg-gray-100  ">
+        <h1 className=" mx-auto max-w-[78vw] m-10 text-7xl font-raleway uppercase font-bold md:text-left text-center">
+          the future <br />
+          of extended reality
+          <br />
+          <span className="lg:text-center mx-auto block">2023</span>
+        </h1>
+        <div>
+          <ButtonCosmic />
+          <ButtonCosmic />
+        </div>
       </section>
     );
 };
